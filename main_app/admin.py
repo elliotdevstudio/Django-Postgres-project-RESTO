82eb20989f3bceeb8e611f1f146b18aaf58997d1
ElliotHarrison <ellioth1029@gmail.com>
2022-12-02 17:43:29 -0800

    registers models in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+
+from .models import Individual, Business, Job_Post, Review
+
+# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Business
+    can_delete = False
+    verbose_plural_name = 'business'
+
+class ProfileInline(admin.StackedInline):
+    model = Individual
+    can_delete = False
+    verbose_plural_name = 'individual'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.register(Business, Job_Post, Individual,Review)
+
+
